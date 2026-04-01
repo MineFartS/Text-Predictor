7d4a2e8d8cf546c353bbc88735571d9fb8109a98
Phil Hunt <philiphuntiv2@gmail.com>
2026-04-01 18:11:18 -0400

    moved Next to __init__

--- __init__.py
+++ __init__.py
@@ -1,7 +1,12 @@
 from philh_myftp_biz.array import List
 from philh_myftp_biz.file import PKL
-from philh_myftp_biz.pc import Path
 from dataclasses import dataclass
+from random import choice
+from philh_myftp_biz.pc import script_dir
+
+thisd = script_dir()
+
+cache: List[Fragment] = List(PKL(thisd.child('dataset.pkl')))
 
 @dataclass
 class Fragment:
@@ -9,4 +14,23 @@ class Fragment:
     curr: None|str = None
     next: None|str = None
 
-cache: List[Fragment] = List(PKL(Path('dataset.pkl')))
+class _Next:
+    
+    @property
+    def _fragments(self):
+        return [f for f in cache if f.next!=None]
+    
+    def _matches(self, word:str) -> list[Fragment]:
+        return [f for f in self._fragments if f.curr==word]
+
+    def random(self, word:str=None) -> None | str:
+
+        if word:
+            matches = self._matches(word)
+        else:
+            matches = self._fragments
+
+        if len(matches) > 0:
+            return choice(matches).next
+
+Next = _Next()
